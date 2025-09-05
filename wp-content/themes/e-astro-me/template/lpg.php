f25281e947ae40780a80ea7c16609cef7c624c1b
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2025-09-05 15:08:23 +0600

    product template done

---
+++ wp-content/themes/e-astro-me/template/lpg.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * Template Name: LPG Template
+ * Description: A custom Overseas page template for the theme.
+ */
+
+get_header(); ?>
+
+     <section>
+                <div class="container pt-130 pb-120">
+                    <div class="section-heading text-center mb-80 mt-80">
+                            <h4 class="sub-heading after-none" data-text-animation="fade-in"
+                                        data-duration="1.5">LPG & LPG Tank List</h4>
+                    </div>
+                    <div class="first-content-wrapper mt-80">
+                        <div class="row g-5 align-items-center">
+                            <div class="col-xl-7 col-lg-5 col-md-12 col-12">
+                                <div class="right-imgg">
+                                    <img class="w-100" src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/lpg-factory.jpg" alt="Lpg img">
+                                </div>
+                            </div>
+                            <div class="col-xl-5 col-lg-7 col-md-12 col-10 text-left pl-40">
+                                <p>Our company specializes in the import and trade of
+                                    high-quality LPG
+                                    (Liquefied
+                                    Petroleum Gas) and LPG tanks, providing safe, efficient, and
+                                    sustainable energy solutions for residential, commercial, and industrial
+                                    applications.
+                                <p class="pt-24 pb-24">
+                                    We source our LPG from trusted global suppliers, ensuring
+                                    superior purity, consistent performance, and compliance with international safety
+                                    standards. Whether you need bulk LPG supply or customized
+                                    tank solutions,</p>
+                                <p> we offer competitive pricing, timely delivery, and expert support.
+                                    Contact us today to discuss your LPG needs!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+            <section class="lpg-gallery mb-120">
+                <div class="container">
+                    <div class="row g-4 machine-grid justify-content-center">
+
+                        <!-- Row 1 -->
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/01.jpeg" alt="Machine 1">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/02.jpeg" alt="Machine 2">
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/03.jpeg" alt="Machine 3">
+                        </div>
+
+                        <!-- Row 2 -->
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/04.jpeg" alt="Knit Shoe">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/05.jpeg" alt="Factory Line">
+                        </div>
+
+                        <!-- Row 3 -->
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/mechine-catalog/6.png" alt="Fabric Stack">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/07.jpeg" alt="Yarn and Fabric">
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/lpg-2.jpg" alt="Machine 1">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/lpg-3.jpg" alt="Machine 2">
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/lpg-6.jpg" alt="Machine 3">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/lpg-5.jpg" alt="Machine 2">
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/lpg-7.jpg" alt="Machine 3">
+                        </div>
+                    </div>
+                </div>
+            </section>
+<?php
+get_footer();
---
+++ wp-content/themes/e-astro-me/template/texttile.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * Template Name: Texttile Template
+ * Description: A custom Overseas page template for the theme.
+ */
+
+get_header(); ?>
+
+     <section>
+                <div class="container pt-130 pb-120">
+                    <div class="section-heading text-center mb-80 mt-80">
+                            <h4 class="sub-heading after-none" data-text-animation="fade-in"
+                                        data-duration="1.5">Textile Commodities</h4>
+                    </div>
+                    <div class="first-content-wrapper mt-80">
+                        <div class="row g-5 align-items-center">
+                            <div class="col-xl-7 col-lg-5 col-md-12 col-12">
+                                <div class="right-imgg">
+                                    <img class="w-100" src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/lpg/texttilee.jpg" alt="Lpg img">
+                                </div>
+                            </div>
+                            <div class="col-xl-5 col-lg-7 col-md-12 col-10 text-left pl-40">
+                                <p>As a leading trader of high-quality textile raw materials, we supply a comprehensive
+                                    range of essential products to support garment factories,
+                                    textile mills, and fabric manufacturers worldwide. Our product portfolio includes
+                                    raw cotton, raw yarn and dyes & Chemicals. We source our
+                                    materials from trusted global producers, ensuring consistent quality,
+                                <p class="pt-24 pb-24">
+                                    competitive pricing, and reliable supply chain management. Our technical
+                                    team provides expert guidance on material selection to optimize your production
+                                    quality and cost-efficiency. </p>
+                                <p> Contact our sales team today to discuss your textile sourcing requirements!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+            <section class="lpg-gallery mb-120">
+                <div class="container">
+                    <div class="row g-4 machine-grid justify-content-center">
+
+                        <!-- Row 1 -->
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/02.jpeg" alt="Machine 2">
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/03.jpeg" alt="Machine 3">
+                        </div>
+
+                        <!-- Row 2 -->
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/04.jpeg" alt="Knit Shoe">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/05.jpeg" alt="Factory Line">
+                        </div>
+
+                        <!-- Row 3 -->
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/06.jpeg" alt="Fabric Stack">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/07.jpeg" alt="Yarn and Fabric">
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/08.jpeg" alt="Machine 1">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/09.jpeg" alt="Machine 2">
+                        </div>
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/10.jpeg" alt="Machine 3">
+                        </div>
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <img src="<?php echo esc_url(get_template_directory_uri( )); ?>/assets/img/dyes-chemicals/11.jpeg" alt="Machine 2">
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+<?php
+get_footer();
